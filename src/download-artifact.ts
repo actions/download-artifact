bdc1124b30fce3a6650a6fc07afa2dcac488ea79
konradpabjan <konradpabjan@outlook.com>
2020-02-20 22:41:17 -0500

    Improve logs

--- src/download-artifact.ts
+++ src/download-artifact.ts
@@ -6,17 +6,32 @@ async function run(): Promise<void> {
   try {
     const name = core.getInput(Inputs.Name, {required: false})
     const path = core.getInput(Inputs.Path, {required: false})
-    const artifactClient = artifact.create()
 
+    const artifactClient = artifact.create()
     if (!name) {
       // download all artifacts
-      await artifactClient.downloadAllArtifacts(path)
+      const downloadResponse = await artifactClient.downloadAllArtifacts(path)
+      core.info(`There were ${downloadResponse.length} artifacts downloaded`)
+      for (const artifact of downloadResponse) {
+        core.info(
+          `Artifact ${artifact.artifactName} was downloaded to ${artifact.downloadPath}`
+        )
+      }
     } else {
       // download a single artifact
-      await artifactClient.downloadArtifact(name, path, {
+      const downloadOptions = {
         createArtifactFolder: false
-      })
+      }
+      const downloadResponse = await artifactClient.downloadArtifact(
+        name,
+        path,
+        downloadOptions
+      )
+      core.info(
+        `Artifact ${downloadResponse.artifactName} was downloaded to ${downloadResponse.downloadPath}`
+      )
     }
+    core.info('Artifact download has finished successfully')
   } catch (err) {
     core.setFailed(err.message)
   }
